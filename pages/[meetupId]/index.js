dd0271f8db8419ef19302252cc79bb60aa8f3d3d
b0jun <bjkim0228@naver.com>
2022-05-29 13:07:18 +0900

    add MeetupDeatil

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://images.unsplash.com/photo-1546436836-07a91091f160?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
+			title="First Meetup"
+			address="Some address 5, 12345 Some City"
+			description="This is a first meetup!"
+		/>
+	);
+}
+
+export default MeetupDetails;
