d3dd72e32ad56bd539d720fc8337ae30295d999d
b0jun <bjkim0228@naver.com>
2022-05-30 12:47:48 +0900

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,42 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
-	return (
-		<MeetupDetail
-			image="https://images.unsplash.com/photo-1546436836-07a91091f160?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"
-			title="First Meetup"
-			address="Some address 5, 12345 Some City"
-			description="This is a first meetup!"
-		/>
-	);
+function MeetupDetails(props) {
+	const { image, title, address, description } = props.meetupData;
+	return <MeetupDetail image={image} title={title} address={address} description={description} />;
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1',
+				},
+			},
+			{
+				params: {
+					meetupId: 'm2',
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	const { meetupId } = context.params;
+
+	return {
+		props: {
+			meetupData: {
+				id: meetupId,
+				image: 'https://images.unsplash.com/photo-1546436836-07a91091f160?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80',
+				title: 'First Meetup',
+				address: 'Some address 5, 12345 Some City',
+				description: 'This is a first meetup!',
+			},
+		},
+	};
 }
 
 export default MeetupDetails;
